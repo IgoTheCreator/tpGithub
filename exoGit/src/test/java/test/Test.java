d1ccb4ad7a53efa1e7178110f8c1c99bad457a34
IgoTheCreator <ni.pinto18@gmail.com>
2023-04-26 10:59:57 +0200

    version 2

--- exoGit/src/test/java/test/Test.java
+++ exoGit/src/test/java/test/Test.java
@@ -1,11 +1,25 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Entreprise;
+import model.Personne;
+
 public class Test {
+	
+	public static void main(String[] args) {
 
 	Personne p1 = new Personne ("Dupond", "Jean");
 	Personne p2 = new Personne ("Titi", "Toto");
 	
-	personnes.add(p1); 
-
-	Entreprise e1 = Entreprise 
+	List<Personne> personnes = new ArrayList();
+	
+	personnes.add(p1);
+	personnes.add(p2);
+	
+	Entreprise e1 = new Entreprise("Sopra", personnes);
+	
+	System.out.println(e1);
+	}
 }
